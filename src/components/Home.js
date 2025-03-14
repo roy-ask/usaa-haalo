a8685b01170e7fdd01014e85f5a07b3df5806667
Shane <sluckenbaugh16@gmail.com>
2025-03-14 12:51:36 -0400

    Update Personas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Footer from './Footer.js'
 import Header from './Header.js';
-// import laptop from '../_img/laptop.jpg'
-import Innovator from '../_img/cards/v2/Innovator.png'
-import Playmaker from '../_img/cards/v2/Playmaker.png'
-import Operative from '../_img/cards/v2/Operative.png'
-import Maverick from '../_img/cards/v2/Maverick.png'
+import Pioneer from '../_img/cards/v2/Pioneer.png'
+import Negotiator from '../_img/cards/v2/Negotiator.png'
+import Promotor from '../_img/cards/v2/Promoter.png'
+import Striver from '../_img/cards/v2/Striver.png'
 
 
 const Home = () => {
@@ -59,40 +58,39 @@ const AboutHAALO = () => {
                 <div className='flex flex-col'>
                     <h3 className='mb-[1rem] header text-center lg:text-left'>What is HAALO?</h3>
                     <p className=" text-lg text-center lg:text-left">HAALO stems from an in-depth exploration of 4 organizational cultures (Adhocracy, Community, Hierarchy, Market), delineating various working styles, motivations, and other key aspects. Through this study, we've developed a comprehensive inventory of statements, focused through a people-centric perspective. This inventory offers insights into how individuals may align within an organization or specific roles.</p>
-                    <img className='rounded-[6px] my-[2rem] w-[90%] lg:hidden mx-auto' src={Innovator} alt='Innovator Persona Card'></img>
+                    <img className='rounded-[6px] my-[2rem] w-[90%] lg:hidden mx-auto' src={Pioneer} alt='Pioneer Persona Card'></img>
                     <h3 className='mb-[1rem] mt-[1rem] header text-center lg:text-left'>The Personas:</h3>
                     <ul className='text-xl list-disc ml-[1.6rem]'>
-                        <li className='mb-[1rem]'><b>Academic</b>: Relating to education and scholarship.</li>
+                    <li className='mb-[1rem]'><b>Scholar</b>: Relating to education and scholarship.</li>
                         <li className='mb-[1rem]'><b>Advocate</b>: A person who publicly supports or recommends a particular cause or policy.</li>
-                        <li className='mb-[1rem]'><b>Analyst</b>: A detailed examination of anything complex in order to understand its nature or to determine its essential features. </li>
-                        <li className='mb-[1rem]'><b>Broker</b>: A person who buys and sells goods or assets for others.</li>
+                        <li className='mb-[1rem]'><b>Scientist</b>: A detailed examination of anything complex in order to understand its nature or to determine its essential features. </li>
+                        <li className='mb-[1rem]'><b>Strategizer</b>: A person who buys and sells goods or assets for others.</li>
                         <li className='mb-[1rem]'><b>Captain</b>: The person in command of the ship.</li>
                         <li className='mb-[1rem]'><b>Chameleon</b>: A person given to often expedient or facile change in ideas or character.</li>
                         <li className='mb-[1rem]'><b>Closer</b>: A person who is skilled at bringing a business transaction to a satisfactory conclusion.</li>
                         <li className='mb-[1rem]'><b>Consultant</b>: A person who provides expert advice professionally.</li>
                         <li className='mb-[1rem]'><b>Counselor</b>: A person trained to give guidance on personal, social, or psychological problems.</li>
-                        <li className='mb-[1rem]'><b>Delegate</b>: A person sent or authorized to represent others, in particular an elected representative sent to a conference.</li>
+                        <li className='mb-[1rem]'><b>Champion</b>: A person sent or authorized to represent others, in particular an elected representative sent to a conference.</li>
                         <li className='mb-[1rem]'><b>Entrepreneur</b>: A person who organizes and operates a business or businesses, taking on  greater than normal financial risks in order to do so.</li>
-                        <li className='mb-[1rem]'><b>Envoy</b>: A messenger or representative, especially one on a diplomatic mission.</li>
+                        <li className='mb-[1rem]'><b>Interpreter</b>: A messenger or representative, especially one on a diplomatic mission.</li>
                         <li className='mb-[1rem]'><b>General</b>: A commander of an army, or an army officer of very high rank.</li>
-                        <li className='mb-[1rem]'><b>Hustler</b>: An aggressively enterprising person; a go-getter.</li>
-                        <li className='mb-[1rem]'><b>Innovator</b>: A person who introduces new methods, ideas, or products.</li>
-                        <li className='mb-[1rem]'><b>Maverick</b>: An an unorthodox or independent-minded person.</li>
+                        <li className='mb-[1rem]'><b>Playmaker</b>: An aggressively enterprising person; a go-getter.</li>
+                        <li className='mb-[1rem]'><b>Pioneer</b>: A person who introduces new methods, ideas, or products.</li>
+                        <li className='mb-[1rem]'><b>Striver</b>: An an unorthodox or independent-minded person.</li>
                         <li className='mb-[1rem]'><b>Mercenary</b>: Primarily concerned with making money at the expense of ethics.</li>
-                        <li className='mb-[1rem]'><b>Operative</b>: A worker, especially a skilled one.</li>
-                        <li className='mb-[1rem]'><b>Playmaker</b>: A player in a team game who leads attacks or brings other players on the same side into a position from which they could score.</li>
-                        <li className='mb-[1rem]'><b>Politician</b>: A a person who is professionally involved in politics, especially as a holder of or a candidate for an elected office.</li>
-                        <li className='mb-[1rem]'><b>Surgeon</b>: A medical practitioner qualified to practice surgery.</li>
-                        <li className='mb-[1rem]'><b>Trainer</b>: A person who teaches others, source of information.</li>
+                        <li className='mb-[1rem]'><b>Promoter</b>: A worker, especially a skilled one.</li>
+                        <li className='mb-[1rem]'><b>Negotiator</b>: A player in a team game who leads attacks or brings other players on the same side into a position from which they could score.</li>
+                        <li className='mb-[1rem]'><b>Diplomat</b>: A a person who is professionally involved in politics, especially as a holder of or a candidate for an elected office.</li>
+                        <li className='mb-[1rem]'><b>Navigator</b>: A medical practitioner qualified to practice surgery.</li>
+                        <li className='mb-[1rem]'><b>Guardian</b>: A person who teaches others, source of information.</li>
                     </ul>
                 </div>
                 <div className='justify-self-end'>
-                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block' src={Innovator} alt='Innovator Persona Card'></img>
-                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block' src={Playmaker} alt='Playmaker Persona Card'></img>
-                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block 2xl:hidden' src={Operative} alt='Playmaker Persona Card'></img>
-                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block xl:hidden' src={Maverick} alt='Playmaker Persona Card'></img>
+                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block' src={Pioneer} alt='Pioneer Persona Card'></img>
+                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block' src={Negotiator} alt='Negotiator Persona Card'></img>
+                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block 2xl:hidden' src={Promotor} alt='Promoter Persona Card'></img>
+                    <img className='rounded-[6px] w-[550px] mb-[5rem] hidden lg:block xl:hidden' src={Striver} alt='Striver Persona Card'></img>
                 </div>
-                {/* <img className='w-[100%] justify-self-end rounded-[40px]' src={laptop}></img> */}
             </div>
     )
 }
